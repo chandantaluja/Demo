15ccbda5471935e9a270d099acc2ed422d7e0512
Chandan Taluja <chandan.taluja@gmail.com>
2023-11-12 00:29:14 +0530

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Demo
 This is my first Git Repository
 <br>
-Author - Chandan Taluja
+Author - Chandan Taluja (Learning with Chandan)
---
+++ src/DemoProject.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : DemoProject.c
+ Author      : Chandan Taluja
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
+	printf("This is the demo project\n");
+	return 0;
+}

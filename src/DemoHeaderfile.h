e5e4ab68148d3dd072afd3fcacc53140d02d76a1
Chandan Taluja <chandan.taluja@gmail.com>
2023-11-12 01:58:42 +0530

    adding the header file

---
+++ src/DemoHeaderfile.h
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : DemoHeaderFile.h
+ Author      : Chandan Taluja
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#ifndef DEMOHEADERFILE_H
+#define DEMOHEADERFILE_H
+/*
+ ============================================================================
+ Function Name        : add
+ Syntax               : int add (int a, int b)
+ Parameter (in)       : int a
+ Parameter (in)       : int b
+ Return               : (a+b)
+ Description : returns the sum of the variables passed to the function
+ ============================================================================
+ */
+int add (int a, int b);
+
+#endif /*DEMOHEADERFILE_H*/
--- src/DemoProject.c
+++ src/DemoProject.c
@@ -8,11 +8,24 @@
  ============================================================================
  */
 
+/* Header File inclusions*/
 #include <stdio.h>
 #include <stdlib.h>
+#include "DemoHeaderFile.h"
 
+#define NUMBER_ONE ((int)10)
+#define NUMBER_TWO ((int)20)
+
+/* Main Function*/
 int main(void) {
 	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
 	printf("This is the demo project\n");
+	printf("sum  = %d\n", add (NUMBER_ONE,NUMBER_TWO));
 	return 0;
+}
+
+/*Sub functions*/
+int add (int a , int b)
+{
+	return a+b;
 }
